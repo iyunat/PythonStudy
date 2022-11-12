feaccaf831294925975967fad48b8f3552a83ef4
Natalya <iyunat@yandex.ru>
2022-11-12 16:31:56 +0300

    Creating Homework10_lesson10 and Descriptor1.py

---
+++ PythonHomework/Homework010_lesson10/Descriptor1.py
@@ -0,0 +1,38 @@
+#Создать не менее двух дескрипторов для атрибутов классов, которые вы создали ранее в ДЗ
+
+class PositiveNumber:
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.my_attr]
+
+    def __set__(self, instance, value):
+        if value < 0:
+            raise ValueError("Не может быть отрицательным")
+        instance.__dict__[self.my_attr] = value
+
+    def __delete__(self, instance):
+        del instance.__dict__[self.my_attr]
+    
+    def __set_name__(self, owner, my_attr):
+        # owner - владелец атрибута - <class '__main__.Worker'>
+        # my_attr - имя атрибута владельца - hours, rate
+        self.my_attr = my_attr
+
+
+class Road:
+    length = PositiveNumber()
+    width = PositiveNumber()
+    weight_square_meter = PositiveNumber()
+    thickness_canvas = PositiveNumber()
+
+    def __init__(self, length, width, weight_square_meter, thickness_canvas ):
+        self.length = length
+        self.width = width
+        self.weight_square_meter = weight_square_meter
+        self.thickness_canvas = thickness_canvas
+        
+    def asphalt_mass (self):
+        print (self.length * self.width * self.weight_square_meter * self.thickness_canvas)
+
+r1 = Road (20, 5000, 25, 0.05)
+
+r1.asphalt_mass()
