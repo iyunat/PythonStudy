c0ce7c58751ce0412294f44207dea7263e027632
Natalya <iyunat@yandex.ru>
2022-11-16 01:39:57 +0300

    Creating Task_02.py

---
+++ PythonHomework/Homework011_lesson11/Lesson_1_Uvanov/Task_02.py
@@ -0,0 +1,10 @@
+prog_1= [b'class', b'function', b'method']
+
+for i in prog_1:
+    
+    print ('Tип переменной:{}'.format(type(i)))
+    print('Cодержание переменной:{}'.format(i))
+    print('Длина переменной: {}'.format (len(i)))
+    print()
+
+
